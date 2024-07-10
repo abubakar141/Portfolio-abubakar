bbb2e2af03080ccabe7561db726c378c3df610b5
abubkar141 <abubakargamerz1415@gmail.com>
2024-07-10 12:44:44 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+import Hero from '@/components/Hero'
+import React from 'react'
+import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
+const page = () => {
+  return (
+    <div>
+    <Hero />
+    <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <Skills />
+      <br /> <br /> <br /> <br /> <br /> <br /> 
+      <Projects />
+    </div>
+  )
+}
+
+export default page
